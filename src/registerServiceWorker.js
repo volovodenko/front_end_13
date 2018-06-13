b74e37228b3d772dfa343d332952f0ddf9dd4a4a
Vitaly Volovodenko <volovodenko@gmail.com>
2018-06-13 10:46:46 +0300

    Patch 6

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -21,7 +21,10 @@ const isLocalhost = Boolean(
 export default function register() {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
-    const publicUrl = new URL(process.env.PUBLIC_URL, window.location);
+    const publicUrl = new URL(process.env.__PATH__, window.location);
+      console.log(publicUrl);
+      console.log(window.location);
+
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
@@ -30,7 +33,7 @@ export default function register() {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = `${process.env.__PATH__}service-worker.js`;
       console.log(swUrl);
 
       if (isLocalhost) {
